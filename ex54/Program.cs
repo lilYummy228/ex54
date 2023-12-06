3adaecad7a290385c0d43e55a016cb5e5d623a84
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-12-06 17:33:41 +0500

    123

--- ex54/Program.cs
+++ ex54/Program.cs
@@ -10,7 +10,112 @@ namespace ex54
     {
         static void Main(string[] args)
         {
+            const string CommandSortByName = "1";
+            const string CommandSortByAge = "2";
+            const string CommandRemoveByDisease = "3";
+            const string CommandExit = "4";
 
+            Hospital hospital = new Hospital();
+            List<Patient> patients = hospital.GivePatients();
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                hospital.ShowPatients(patients);
+
+                Console.WriteLine($"Больница\n" +
+                    $"{CommandSortByName} - отсортировать больных по имени\n" +
+                    $"{CommandSortByAge} - отсортировать больных по возрасту\n" +
+                    $"{CommandRemoveByDisease} - вывести больных с заболеванием\n" +
+                    $"{CommandExit} - выйти из программы\n" +
+                    $"Ваш ввод: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        hospital.SortByName(ref patients);
+                        break;
+
+                    case "2":
+                        hospital.SortByAge(ref patients);
+                        break;
+
+                    case "3":
+                        hospital.RemoveByDisease(ref patients);
+                        break;
+
+                    case "4":
+                        isOpen = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный ввод...");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+    class Hospital
+    {
+        private List<Patient> _patients = new List<Patient>();
+
+        public Hospital()
+        {
+            AddPatients();
+        }
+
+        public void SortByName(ref List<Patient> patients)
+        {
+            var sortedPatients = _patients.OrderBy(patient => patient.Name).ToList();
+            patients = sortedPatients;
+        }
+
+        public void SortByAge(ref List<Patient> patients)
+        {
+            var sortedPatients = _patients.OrderBy(patient => patient.Age).ToList();
+            patients = sortedPatients;
+        }
+
+        public void RemoveByDisease(ref List<Patient> patients)
+        {
+            Console.WriteLine("Напишите название заболевания: ");
+            string disease = Console.ReadLine();
+
+            var removedPatients = _patients.Where(patient => patient.Disease != disease).ToList();
+            patients = removedPatients;
+        }
+
+        public List<Patient> GivePatients()
+        {
+            return _patients;
+        }
+
+        public void ShowPatients(List<Patient> patients)
+        {
+            Console.SetCursorPosition(0, 12);
+            Console.WriteLine("Список больных: ");
+
+            for (int i = 0; i < patients.Count; i++)
+            {
+                patients[i].ShowInfo();
+            }
+
+            Console.SetCursorPosition(0, 0);
+        }
+
+        private void AddPatients()
+        {
+            _patients.Add(new Patient("Мыларщиков Илья Константинович", 22, "Альцгеймер"));
+            _patients.Add(new Patient("Ступников Роман Дмитриевич", 25, "Гипертония"));
+            _patients.Add(new Patient("Васьков Роман Олегович", 30, "Ушной атит"));
+            _patients.Add(new Patient("Богатырев Иван Олегович", 18, "Маленькость"));
+            _patients.Add(new Patient("Ларионов Егор Александрович", 40, "Сердечная недостаточность"));
+            _patients.Add(new Patient("Красовский Михаил Евгеньевич", 52, "Рак легких"));
+            _patients.Add(new Patient("Волосников Вениамин Олегович", 12, "Простуда"));
         }
     }
 
@@ -26,5 +131,10 @@ namespace ex54
         public string Name { get; private set; }
         public int Age { get; private set; }
         public string Disease { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name}. Возраст: {Age} лет. Заболевание: {Disease}");
+        }
     }
 }
